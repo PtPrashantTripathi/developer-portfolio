03ca868e8e195b90a676ad04807a29eefb363115
ptprashanttripathi <pt8435782545@gmail.com>
2019-05-12 22:56:53 +0530

    NEW files added

---
+++ footer.php
@@ -0,0 +1,42 @@
+<footer class="page-footer" style="background:#212121;">
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-6">
+                <br><p class="title1">Developer : Pt. Prashant Triapthi </p>
+                <p class="title2">Credits   : 
+					<a href="https://Google.com" style="color:#fff">Google</a>,
+					<a href="https://quora.com" style="color:#fff">Quora</a>,
+					<a href="https://stackoverflow.com" style="color:#fff">Stack Overflow</a>,
+					<a href="http://github.com" style="color:#fff">Github</a>
+				</p>
+            </div>
+            <div class="col-xs-12 col-sm-6">
+				<a href="https://www.facebook.com/PtPrashantTripathi" class="fa fa-facebook" style="font-size: 25px;"></a>
+				<a href="https://twitter.com/PtPrashant09" class="fa fa-twitter" style="font-size: 25px;"></a>
+				<a href="https://plus.google.com/u/0/117932919291115315316" class="fa fa-google" style="font-size: 25px;"></a>
+				<a href="https://www.linkedin.com/in/ptprashanttripathi" class="fa fa-linkedin" style="font-size: 25px;"></a>
+				<a href="https://www.instagram.com/PtPrashantTripathi" class="fa fa-instagram" style="font-size: 25px;"></a>
+				<a href="https://www.github.com/PtPrashantTripathi" class="fa fa-github" style="font-size: 25px;"></a>
+			</div>
+		</div>
+    </div>
+    <div class="footer-copyright">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <center>Pt. Prashant Tripathi © 2015 - <?php echo date("Y"); ?> All Rights Reserved. </center>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+
+<!--essential javascript for website-->
+
+<script src="/js/jquery.mCustomScrollbar.concat.min.js"></script>
+<script src="/js/bootstrap.min.js"></script>
+<script src="/js/material.min.js"></script>
+<script src="/js/main.js"></script>
+</body>
+
+</html>
---
+++ portfolio/src/index.php
@@ -0,0 +1,70 @@
+<?php 
+$url=$_SERVER['REQUEST_URI'];
+
+       $title='Pt. Prashant Triapthi :: Smart Resistance Calculator ';
+       $nav=3;
+       include '../../header.php';
+?>
+
+    <div class="container">
+
+        <div class="row">
+
+            <div class="col-xs-12 ">
+                <div class="panel panel-primary">
+                    <div class="panel-heading panel-heading-custom" style="background: #F44336; color: #fff; font-size:20px;">Smart Resistance Calculator <a href="/ portfolio/" style="color:#fff"><span class="pull-right hidden-xs">View more projects</span></a></div>
+                    <div class="panel-body" style="padding:50px;">
+                        <div class="row">
+                            <div class="col-xs-12 col-sm-6"> 
+								<p class="title1">Smart Resistance Calculator</p>
+                                <p>A android app which provide user to calc the Values of Resister.</p>
+								<p>It is Simpaly functional android app</p>
+                                <p>To view source code & live working of this project <a href="https://pt-prashant-tripathi.github.io/Smart-Resisrance-Calculator/" style="color:blue;" target="_blank">View on github.io</a></p>
+                                <p>And Click Hare to downlode this project <a href="https://drive.google.com/open?id=0B4jxiBRriV_lQjM4dHktTmIxN3c" style="color:blue;" target="_blank">Smart-Resisrance-Calculator.apk</a></p>
+                            </div>
+
+                             <div class="col-xs-12 col-sm-6">
+								<img src="/image/src1.png" class="img img-thumbnail img-responsive" style="height:240px;width:135px;margin:10px;">
+                                <img src="/image/src2.png" class="img img-thumbnail img-responsive" style="height:240px;width:135px;margin:10px;">
+                            </div>
+
+                        </div>
+                        <br>
+                        <br>
+
+                        <div class="row">
+                            <div class="col-xs-12 col-sm-6">
+                                <p class="title1">Features</p>
+                                <hr width="30%" style="margin:0px">
+                                <br>
+                                <!--quick fact start-->
+                                <p>✔&nbsp;Completely Offline Resister value Calculator.</p>
+                                <p>✔&nbsp;Responsive Interface.</p>
+                                <p>✔&nbsp;Includes Complete guide.</p>
+                                <p>✔&nbsp;Easy to use.</p>
+                            </div>
+
+                            <div class="col-xs-12 col-sm-6">
+                                <p class="title1">Technology use</p>
+                                <hr width="30%" style="margin:0px">
+                                <br>
+                                <!--quick fact start-->
+                                <!--quick fact start-->                                
+								<p>✔&nbsp;Android SDK.</p>
+								<p>✔&nbsp;HTML5,CSS3,jQuery & bootstrap for front-end development.</p>
+                                <p>✔&nbsp;Java for backend.</p>
+                                <p>✔&nbsp;Apache server for testing and development.</p>
+                                <p>✔&nbsp;Git for version control.</p>
+                                <p>✔&nbsp;Notepade++ text for coding.</p>
+                           </div>
+
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+    <?php include '../../footer.php' ;?>
